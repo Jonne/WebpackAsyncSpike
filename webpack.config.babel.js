86932dfdf0bbd7b6fec824c3645fdac3c01efcca
Jonne Kats <joka@avsol.onmicrosoft.com>
2017-02-20 11:15:27 +0100

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';  
+
+export default {  
+  entry: [
+    path.join(__dirname, 'src/index.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [    
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [
+                ['es2015', { modules: false }],
+                'react'
+              ],
+            }
+          }
+        ]
+      }
+    ]
+  } 
+};
